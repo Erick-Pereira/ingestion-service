746af77c89609612b8fd642d8509e723a58ac539
DOSKTOP\Shelow <theshelowgamer@gmail.com>
2026-03-17 22:59:27 -0300

    no message

---
+++ Simcag.IngestionService.Infrastructure/Messaging/RabbitMqPublisher.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+public class RabbitMqPublisher
+{
+    private readonly IModel _channel;
+
+    public RabbitMqPublisher()
+    {
+        var factory = new ConnectionFactory { HostName = "rabbitmq" };
+        var connection = factory.CreateConnection();
+        _channel = connection.CreateModel();
+
+        _channel.QueueDeclare("data.ingested", true, false, false);
+    }
+
+    public Task PublishAsync<T>(T message)
+    {
+        var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+
+        _channel.BasicPublish(
+            "",
+            "data.ingested",
+            null,
+            body
+        );
+
+        return Task.CompletedTask;
+    }
+}
